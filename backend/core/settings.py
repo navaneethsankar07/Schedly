3e51be7a3b6b71005bd4ea8938ba9008f39b26b1
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-05-08 12:25:56 +0530

    chore: add requirements.txt for backend deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,10 +56,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
