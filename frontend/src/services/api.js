8017b7ec24d5fd9b272e77c39fe225f7957619c6
Navaneeth Sankar <navaneethsankar07@gmail.com>
2026-05-08 22:03:10 +0530

    chore: update API base URL for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/',
+  baseURL: 'https://schedly-urut.onrender.com/api/',
 });
 
 api.interceptors.request.use((config) => {
